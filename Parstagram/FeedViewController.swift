afefcdadff59dad920a14f867692ff606dfad169
eluzung <luzungelaine@gmail.com>
2022-10-12 17:38:00 -0400

    Added all required stories

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,15 +6,57 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var feedTableView: UITableView!
+    var posts = [PFObject]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.feedTableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = feedTableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = (post["caption"] as! String)
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.feedPhotoView.af.setImage(withURL: url)
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
