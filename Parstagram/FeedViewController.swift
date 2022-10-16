2a358d413853f429ed8ffd700b124751266cd646
eluzung <luzungelaine@gmail.com>
2022-10-15 22:55:23 -0400

    User story, displaying comments

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -14,7 +14,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var feedTableView: UITableView!
     var posts = [PFObject]()
     let myRefreshControl = UIRefreshControl()
-    var numOfPosts = 5
+    var numOfPosts = 20
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +30,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.order(byDescending: "createdAt")
         query.limit = numOfPosts
         
@@ -54,11 +54,12 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
     }
     
+    /*
     //Infinite scroll feature, loads more Posts
     func loadMorePosts() {
         numOfPosts += 5
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.order(byDescending: "createdAt")
         query.limit = numOfPosts
         
@@ -75,25 +76,46 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             loadMorePosts()
         }
     }
+    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return posts.count
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        return comments.count + 1
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = feedTableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-        let post = posts[indexPath.row]
-        
-        let user = post["author"] as! PFUser
-        cell.usernameLabel.text = user.username
-        cell.captionLabel.text = (post["caption"] as! String)
-        
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        cell.feedPhotoView.af.setImage(withURL: url)
-        
-        return cell
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+
+        if indexPath.row == 0 {
+            let cell = feedTableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+            
+            let user = post["author"] as! PFUser
+            cell.usernameLabel.text = user.username
+            cell.captionLabel.text = (post["caption"] as! String)
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            cell.feedPhotoView.af.setImage(withURL: url)
+            
+            return cell
+        } else {
+            let cell = feedTableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = (comment["text"] as! String)
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
